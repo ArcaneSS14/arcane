aa03bbcd4da1b667b9230bc0e54c718f89d4df52
clopiks <daniklopd@gmail.com>
2026-06-20 21:11:07 +0300

    Исправление N7
    исправляю ошибки от зайчика

--- Content.Shared/_Arcane/Leash/Components/CollarWearerComponent.cs
+++ Content.Shared/_Arcane/Leash/Components/CollarWearerComponent.cs
@@ -1,7 +1,8 @@
 namespace Content.Shared._Arcane.Leash.Components;
 
-[RegisterComponent]
+[RegisterComponent, AutoGenerateComponentState]
 public sealed partial class CollarWearerComponent : Component
 {
+    [AutoNetworkedField]
     public EntityUid? Collar;
 }
