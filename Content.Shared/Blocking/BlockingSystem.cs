space-syndicate/Goob-Station

Фикс. Кто? Патч (#1022)

Content.Shared/Blocking/BlockingSystem.cs
@@ -179,7 +179,7 @@ public sealed partial class BlockingSystem : EntitySystem
             var mobQuery = GetEntityQuery<MobStateComponent>();
             foreach (var uid in intersecting)
             {
-                if (uid != user && mobQuery.HasComponent(uid))
+                if (uid != user && mobQuery.HasComponent(uid) && Transform(uid).ParentUid == xform.ParentUid) // CorvaxGoob-Fixes
                 {
                     TooCloseError(user);
                     return false;

Content.Shared/Movement/Systems/MovementModStatusSystem.cs
@@ -179,6 +179,7 @@ public sealed class MovementModStatusSystem : EntitySystem
 
         status.Comp.SprintSpeedModifier = sprintSpeedModifier;
         status.Comp.WalkSpeedModifier = walkSpeedModifier;
+        Dirty(status); // CorvaxGoob-Fixes
 
         _movementSpeedModifier.RefreshMovementSpeedModifiers(uid);
 

Content.Shared/SubFloor/SharedTrayScannerSystem.cs
@@ -6,6 +6,7 @@ using Content.Shared.Interaction;
 using Content.Shared.Inventory.Events;
 using Robust.Shared.GameStates;
 using Robust.Shared.Network;
+using Robust.Shared.Player; // CorvaxGoob-Fixes
 using Robust.Shared.Serialization;
 
 namespace Content.Shared.SubFloor;
@@ -32,6 +33,12 @@ public abstract class SharedTrayScannerSystem : EntitySystem
         SubscribeLocalEvent<TrayScannerComponent, GotUnequippedEvent>(OnTrayUnequipped);
 
         SubscribeLocalEvent<TrayScannerUserComponent, GetVisMaskEvent>(OnUserGetVis);
+        SubscribeLocalEvent<TrayScannerUserComponent, PlayerAttachedEvent>(OnPlayerAttached); // CorvaxGoob-Fixes
+    }
+
+    private void OnPlayerAttached(Entity<TrayScannerUserComponent> ent, ref PlayerAttachedEvent args) // CorvaxGoob-Fixes
+    {
+        _eye.RefreshVisibilityMask(ent.Owner);
     }
 
     private void OnUserGetVis(Entity<TrayScannerUserComponent> ent, ref GetVisMaskEvent args)

ArcaneSS14/arcane

Content.Shared/Blocking/BlockingSystem.cs
@@ -177,7 +177,7 @@ public sealed partial class BlockingSystem : EntitySystem
             var mobQuery = GetEntityQuery<MobStateComponent>();
             foreach (var uid in intersecting)
             {
-                if (uid != user && mobQuery.HasComponent(uid))
+                if (uid != user && mobQuery.HasComponent(uid) && Transform(uid).ParentUid == xform.ParentUid) // Arcane-Edit
                 {
                     TooCloseError(user);
                     return false;

Content.Shared/Movement/Systems/MovementModStatusSystem.cs
@@ -179,6 +179,7 @@ public sealed class MovementModStatusSystem : EntitySystem
 
         status.Comp.SprintSpeedModifier = sprintSpeedModifier;
         status.Comp.WalkSpeedModifier = walkSpeedModifier;
+        Dirty(status); // Arcane-Edit
 
         _movementSpeedModifier.RefreshMovementSpeedModifiers(uid);
 

Content.Shared/SubFloor/SharedTrayScannerSystem.cs
@@ -6,6 +6,7 @@ using Content.Shared.Interaction;
 using Content.Shared.Inventory.Events;
 using Robust.Shared.GameStates;
 using Robust.Shared.Network;
+using Robust.Shared.Player; // Arcane-Edit
 using Robust.Shared.Serialization;
 
 namespace Content.Shared.SubFloor;
@@ -32,6 +33,12 @@ public abstract class SharedTrayScannerSystem : EntitySystem
         SubscribeLocalEvent<TrayScannerComponent, GotUnequippedEvent>(OnTrayUnequipped);
 
         SubscribeLocalEvent<TrayScannerUserComponent, GetVisMaskEvent>(OnUserGetVis);
+        SubscribeLocalEvent<TrayScannerUserComponent, PlayerAttachedEvent>(OnPlayerAttached); // Arcane-Edit
+    }
+
+    private void OnPlayerAttached(Entity<TrayScannerUserComponent> ent, ref PlayerAttachedEvent args) // Arcane-Edit
+    {
+        _eye.RefreshVisibilityMask(ent.Owner);
     }
 
     private void OnUserGetVis(Entity<TrayScannerUserComponent> ent, ref GetVisMaskEvent args)