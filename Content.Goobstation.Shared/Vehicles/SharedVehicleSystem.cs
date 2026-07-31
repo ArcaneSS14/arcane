space-syndicate/Goob-Station

patchfix (#1013)

Content.Goobstation.Shared/Vehicles/SharedVehicleSystem.cs
@@ -20,6 +20,7 @@ using Content.Shared.Destructible;
 using Content.Goobstation.Maths.FixedPoint;
 using Content.Shared.Damage;
 using Content.Shared.Actions.Components;
+using Content.Shared.Gravity; // CorvaxGoob
 
 namespace Content.Goobstation.Shared.Vehicles;
 
@@ -33,6 +34,7 @@ public abstract partial class SharedVehicleSystem : EntitySystem
     [Dependency] private readonly SharedMoverController _mover = default!;
     [Dependency] private readonly SharedVirtualItemSystem _virtualItem = default!;
     [Dependency] private readonly SharedTransformSystem _transform = default!;
+    [Dependency] private readonly SharedGravitySystem _gravity = default!; // CorvaxGoob
 
     private static readonly EntProtoId HornActionId = "ActionHorn";
     private static readonly EntProtoId SirenActionId = "ActionSiren";
@@ -56,13 +58,24 @@ public abstract partial class SharedVehicleSystem : EntitySystem
         SubscribeLocalEvent<VehicleComponent, BreakageEventArgs>(OnBreak);
         SubscribeLocalEvent<VehicleComponent, DamageChangedEvent>(OnRepair);
         SubscribeLocalEvent<VehicleComponent, GetAdditionalAccessEvent>(OnGetAdditionalAccess);
+        SubscribeLocalEvent<VehicleComponent, RefreshWeightlessModifiersEvent>(OnRefreshWeightlessModifiers); // CorvaxGoob
     }
 
     private void OnInit(EntityUid uid, VehicleComponent component, ComponentInit args)
     {
+        EnsureComp<GravityAffectedComponent>(uid); // CorvaxGoob
+        _gravity.RefreshWeightless(uid); // CorvaxGoob
+
         _appearance.SetData(uid, VehicleState.Animated, component.EngineRunning);
         _appearance.SetData(uid, VehicleState.DrawOver, false);
     }
+
+    // CorvaxGOob-Start
+    private void OnRefreshWeightlessModifiers(EntityUid uid, VehicleComponent component, ref RefreshWeightlessModifiersEvent args)
+    {
+        args.WeightlessModifier = 0;
+    }
+    // CorvaxGoob-End
 
     private void OnRemove(EntityUid uid, VehicleComponent component, ComponentRemove args)
     {

Content.Server/Dragon/DragonSystem.cs
@@ -291,6 +291,8 @@ public sealed partial class DragonSystem : EntitySystem
         if (!Resolve(uid, ref comp))
             return;
 
+        DeleteRifts(uid, true, comp); // CorvaxGoob
+
         // We can't predict the rift being destroyed anyway so no point adding weakened to shared.
         comp.WeakenedAccumulator = comp.WeakenedDuration;
         _movement.RefreshMovementSpeedModifiers(uid);

Content.Server/Holosign/HolosignSystem.cs
@@ -73,7 +73,7 @@ public sealed class HolosignSystem : EntitySystem
             if (_tag.HasTag(entity, component.HolosignTag))
                 return;
 
-            if (!_physicsQuery.TryComp(entity, out var physics))
+            if (!_physicsQuery.TryComp(entity, out var physics) || !physics.CanCollide || !physics.Hard) // CorvaxGoob
                 continue;
 
             if ((physics.CollisionLayer &

Content.Server/_Shitmed/ItemSwitch/ItemSwitchSystem.cs
@@ -44,7 +44,7 @@ public sealed class ItemSwitchSystem : SharedItemSwitchSystem
         if (ent.Comp.State == ent.Comp.DefaultState)
             return;
 
-        var count = (int) (battery.LastCharge / state.EnergyPerUse);
+        var count = _battery.GetRemainingUses((ent.Owner, battery), state.EnergyPerUse); // CorvaxGoob
         args.PushMarkup(Loc.GetString("melee-battery-examine", ("color", "yellow"), ("count", count)));
     }
 
@@ -55,7 +55,7 @@ public sealed class ItemSwitchSystem : SharedItemSwitchSystem
             || !component.States.TryGetValue(component.State, out var state))
             return;
 
-        component.IsPowered = battery.LastCharge >= state.EnergyPerUse;
+        component.IsPowered = _battery.GetCharge((uid, battery)) >= state.EnergyPerUse; // CorvaxGoob
 
         if (component is { IsPowered: false, DefaultState: { } defaultState } && component.State != defaultState)
             _itemSwitch.Switch((uid, component), defaultState);

Content.Shared/_DV/Construction/UpgradeKitSystem.cs
@@ -80,7 +80,7 @@ public sealed class UpgradeKitSystem : EntitySystem
     public bool CanUpgrade(Entity<UpgradeKitComponent> ent, EntityUid target, EntityUid user)
     {
         if (_whitelist.IsWhitelistFail(ent.Comp.Whitelist, target) ||
-            _whitelist.IsWhitelistFail(ent.Comp.Blacklist, target))
+            _whitelist.IsWhitelistPass(ent.Comp.Blacklist, target)) // CorvaxGoob Fail -> Pass
         {
             _popup.PopupClient(Loc.GetString("upgrade-kit-invalid-target"), target, user);
             return false;

ArcaneSS14/arcane

Content.Goobstation.Shared/Vehicles/SharedVehicleSystem.cs
@@ -19,6 +19,7 @@ using Content.Shared.Destructible;
 using Content.Goobstation.Maths.FixedPoint;
 using Content.Shared.Damage;
 using Content.Shared.Actions.Components;
+using Content.Shared.Gravity; // Arcane
 
 namespace Content.Goobstation.Shared.Vehicles;
 
@@ -32,7 +33,7 @@ public abstract partial class SharedVehicleSystem : EntitySystem
     [Dependency] private readonly SharedMoverController _mover = default!;
     [Dependency] private readonly SharedVirtualItemSystem _virtualItem = default!;
     [Dependency] private readonly SharedTransformSystem _transform = default!;
-
+    [Dependency] private readonly SharedGravitySystem _gravity = default!; // Arcane
     private static readonly EntProtoId HornActionId = "ActionHorn";
     private static readonly EntProtoId SirenActionId = "ActionSiren";
 
@@ -55,13 +56,24 @@ public abstract partial class SharedVehicleSystem : EntitySystem
         SubscribeLocalEvent<VehicleComponent, BreakageEventArgs>(OnBreak);
         SubscribeLocalEvent<VehicleComponent, DamageChangedEvent>(OnRepair);
         SubscribeLocalEvent<VehicleComponent, GetAdditionalAccessEvent>(OnGetAdditionalAccess);
+        SubscribeLocalEvent<VehicleComponent, RefreshWeightlessModifiersEvent>(OnRefreshWeightlessModifiers); // Arcane
     }
 
     private void OnInit(EntityUid uid, VehicleComponent component, ComponentInit args)
     {
+        EnsureComp<GravityAffectedComponent>(uid); // Arcane
+        _gravity.RefreshWeightless(uid); // Arcane
+
         _appearance.SetData(uid, VehicleState.Animated, component.EngineRunning);
         _appearance.SetData(uid, VehicleState.DrawOver, false);
     }
+
+    // Arcane-Start
+    private void OnRefreshWeightlessModifiers(EntityUid uid, VehicleComponent component, ref RefreshWeightlessModifiersEvent args)
+    {
+        args.WeightlessModifier = 0;
+    }
+    // Arcane-End
 
     private void OnRemove(EntityUid uid, VehicleComponent component, ComponentRemove args)
     {

Content.Server/Dragon/DragonSystem.cs
@@ -287,6 +287,8 @@ public sealed class DragonSystem : EntitySystem
         if (!Resolve(uid, ref comp))
             return;
 
+        DeleteRifts(uid, true, comp); // Arcane
+
         // We can't predict the rift being destroyed anyway so no point adding weakened to shared.
         comp.WeakenedAccumulator = comp.WeakenedDuration;
         _movement.RefreshMovementSpeedModifiers(uid);

Content.Server/Holosign/HolosignSystem.cs
@@ -73,7 +73,7 @@ public sealed class HolosignSystem : EntitySystem
             if (_tag.HasTag(entity, component.HolosignTag))
                 return;
 
-            if (!_physicsQuery.TryComp(entity, out var physics))
+            if (!_physicsQuery.TryComp(entity, out var physics) || !physics.CanCollide || !physics.Hard) // Arcane
                 continue;
 
             if ((physics.CollisionLayer &

Content.Server/_Shitmed/ItemSwitch/ItemSwitchSystem.cs
@@ -44,7 +44,7 @@ public sealed class ItemSwitchSystem : SharedItemSwitchSystem
         if (ent.Comp.State == ent.Comp.DefaultState)
             return;
 
-        var count = (int) (battery.LastCharge / state.EnergyPerUse);
+        var count = _battery.GetRemainingUses((ent.Owner, battery), state.EnergyPerUse); // Arcane
         args.PushMarkup(Loc.GetString("melee-battery-examine", ("color", "yellow"), ("count", count)));
     }
 
@@ -55,7 +55,7 @@ public sealed class ItemSwitchSystem : SharedItemSwitchSystem
             || !component.States.TryGetValue(component.State, out var state))
             return;
 
-        component.IsPowered = battery.LastCharge >= state.EnergyPerUse;
+        component.IsPowered = _battery.GetCharge((uid, battery)) >= state.EnergyPerUse; // Arcane
 
         if (component is { IsPowered: false, DefaultState: { } defaultState } && component.State != defaultState)
             _itemSwitch.Switch((uid, component), defaultState);

Content.Shared/_DV/Construction/UpgradeKitSystem.cs
@@ -80,7 +80,7 @@ public sealed class UpgradeKitSystem : EntitySystem
     public bool CanUpgrade(Entity<UpgradeKitComponent> ent, EntityUid target, EntityUid user)
     {
         if (_whitelist.IsWhitelistFail(ent.Comp.Whitelist, target) ||
-            _whitelist.IsWhitelistFail(ent.Comp.Blacklist, target))
+            _whitelist.IsWhitelistPass(ent.Comp.Blacklist, target)) // Arcane Fail -> Pass
         {
             _popup.PopupClient(Loc.GetString("upgrade-kit-invalid-target"), target, user);
             return false;