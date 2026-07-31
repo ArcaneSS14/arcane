ebef4348aea38ff222710b970555ce8b46aa46a8
nenado45 <werj546@mail.ru>
2026-07-31 14:23:23 +0500

    Update StutteringSystem.cs

--- Content.Server/Speech/EntitySystems/StutteringSystem.cs
+++ Content.Server/Speech/EntitySystems/StutteringSystem.cs
@@ -25,7 +25,7 @@ namespace Content.Server.Speech.EntitySystems
         [Dependency] private readonly IRobustRandom _random = default!;
 
         // Regex of characters to stutter.
-        private static readonly Regex Stutter = new(@"[b-df-hj-np-tv-wxyz-б-вд-к-лмн-прст]",
+        private static readonly Regex Stutter = new(@"[b-df-hj-np-tv-wxyz-б-вд-к-лмн-прст]", // Arcane-Edit
             RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
         public override void Initialize()
