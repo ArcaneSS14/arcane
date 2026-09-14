7052f1bda13cfe67102856bd2aae05c466616aa4
ReWAFFlution <rockstarfly65@gmail.com>
2026-09-15 00:51:25 +0300

    berserk buff

--- Content.Goobstation.Shared/BerserkerImplant/BerserkerImplantActiveComponent.cs
+++ Content.Goobstation.Shared/BerserkerImplant/BerserkerImplantActiveComponent.cs
@@ -26,7 +26,7 @@ public sealed partial class BerserkerImplantActiveComponent : Component
     public float StunModifier = 0.3f; // Arcane-Edit: 0.5 -> 0.3
 
     [ViewVariables(VVAccess.ReadWrite)]
-    public float SelfDamageModifier = 1.5f;
+    public float SelfDamageModifier = 1.7f; // Arcane-Edit: 1.5 -> 1.7
 
     [ViewVariables(VVAccess.ReadWrite)]
     public DamageSpecifier DelayedDamage = new();
