b9261aee9b2f624ab639ff594356b2fd9df88389
UmbiMax <109347499+UmbiMax@users.noreply.github.com>
2026-09-15 18:57:01 +0500

    Only one aphro

--- Content.Server/StationEvents/Components/VentClogRuleComponent.cs
+++ Content.Server/StationEvents/Components/VentClogRuleComponent.cs
@@ -17,7 +17,7 @@ public sealed partial class VentClogRuleComponent : Component
     [DataField]
     public IReadOnlyList<ProtoId<ReagentPrototype>> SafeishVentChemicals = new ProtoId<ReagentPrototype>[]
     {
-        "Water", "Blood", "Slime", "SpaceDrugs", "SpaceCleaner", "Nutriment", "Sugar", "SpaceLube", "Ephedrine", "Ale", "Beer", "SpaceGlue", "Aphrodisiac", "Aphrodisiac"  // Arcane-Edit: Double chance for ahpro 
+        "Water", "Blood", "Slime", "SpaceDrugs", "SpaceCleaner", "Nutriment", "Sugar", "SpaceLube", "Ephedrine", "Ale", "Beer", "SpaceGlue", "Aphrodisiac" // Arcane-Edit: Double chance for ahpro 
     };
 
     /// <summary>
