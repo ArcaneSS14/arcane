e00bca59d77e72d1aa922a7ad105c81b80415d15
Just_Useless <Amir5567ai@yandex.ru>
2026-09-15 10:45:56 +0500

    double*

--- Content.Server/StationEvents/Components/VentClogRuleComponent.cs
+++ Content.Server/StationEvents/Components/VentClogRuleComponent.cs
@@ -17,7 +17,7 @@ public sealed partial class VentClogRuleComponent : Component
     [DataField]
     public IReadOnlyList<ProtoId<ReagentPrototype>> SafeishVentChemicals = new ProtoId<ReagentPrototype>[]
     {
-        "Water", "Blood", "Slime", "SpaceDrugs", "SpaceCleaner", "Nutriment", "Sugar", "SpaceLube", "Ephedrine", "Ale", "Beer", "SpaceGlue", "Aphrodisiac", "Aphrodisiac"  /* Arcane, doube chance for ahpro */
+        "Water", "Blood", "Slime", "SpaceDrugs", "SpaceCleaner", "Nutriment", "Sugar", "SpaceLube", "Ephedrine", "Ale", "Beer", "SpaceGlue", "Aphrodisiac", "Aphrodisiac"  // Arcane, double chance for ahpro 
     };
 
     /// <summary>
